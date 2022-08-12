05d67db98bbf0d4f40803c0299da7662725467c4
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2022-08-12 16:53:04 +0530

    setting up project

---
+++ src/main/java/com/innovative/webService/DiaryWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innovative.webService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryWebServiceApplication.class, args);
+	}
+
+}
